57896dfbb144fed6ac04c9d3719711affe6edc53
ruggz13 <nicolasruggieri7@gmail.com>
2019-07-11 17:01:13 -0400

    CLI working with no bugs

--- run.rb
+++ run.rb
@@ -6,48 +6,112 @@ require_relative "./app/models/organization.rb"
 require_relative "./app/models/sector.rb"
 require_relative "./app/models/department.rb"
 require_relative "./db/seeds.rb"
+
+def help_method
+	puts "Here is a list of all inputs avaiable:"
+	puts ""
+	puts "Job Inputs: "
+	puts ""
+	puts "1. Find highest paying job"
+	puts "2. Search jobs by a location"
+	puts "3. Search jobs by full time status"
+	puts "4. Seach jobs by part time status"
+	puts ""
+	puts "Organization Inputs: "
+	puts "5. Find the average salary by an organization"
+	puts ""
+	puts "Trouble Shooting Inputs: "
+	puts "quit"
+	puts "help"
+end
+
 def welcome_message
-        puts "Welcome to our Job Analyzer!"
-        end
+    puts "Welcome to our Job Analyzer!"
+    puts ""
+    help_method
+end
+
+def get_input
+	input = false
+	while true
+		valid = ["","find highest paying job","1.","1","one", "search jobs by full time status","3.","3","three","seach jobs by part time status","4.", "four","find the average salary by organization","5.","5","five", "help", "quit"]
+		puts ""
+		if valid.include?(input)
+			puts "Please enter a given input or number:"
+		else 
+			puts "Please enter a valid input:"
+		end
+		input = gets.chomp.downcase
+			if input == "find highest paying job" || input == "1." || input == "1" || input == "one"
+				puts ""
+				print Job.highest_pay
+				puts ""
+			# elsif input == "Search jobs by location"
+				# print Job.search_by_location("Klockoburgh")
+			elsif input == "search jobs by full time status" || input == "3." || input == "3" || input == "three"
+				puts ""
+				puts Job.search_by_full_time
+				puts ""
+			elsif input == "seach jobs by part time status" || input == "4." || input == "4" || input == "four"
+				puts ""
+				puts Job.search_by_part_time
+				puts ""
+			elsif input == "find the average salary by organization" || input == "5." || input == "5" || input == "five"
+				puts ""
+				print "$#{Organization.all[1].avg_sal_org}"
+				puts ""
+			elsif input == "help"
+				puts "\n***************************\n"
+				help_method
+				puts "\n***************************\n"
+				input = ""
+			elsif input == "quit"
+				exit
+			end
+	end	
+			
+			
+end
 
 
 
-gen_data
-# welcome_message
-puts ""
-puts "*************"
-puts "Highest pay"
-puts "*************"
-puts ""
-print Job.highest_pay
-puts ""
-puts "*************"
-puts "Search by location"
-puts "*************"
-print Job.search_by_location("Klockoburgh")
-puts ""
-puts "*************"
-puts "Search by full time"
-puts "*************"
-puts ""
-# binding.pry
-puts Job.search_by_full_time
-puts ""
-puts "*************"
-puts "Search by part time "
-puts "*************"
-puts ""
-puts Job.search_by_part_time
-puts ""
-puts "*************"
-puts ""
-# puts Job.highest_pay_part_time
-puts ""
-puts "*************"
-puts ""
-puts ""
-puts "*************"
-print "$#{Organization.first.avg_sal_org}"
-puts ""
-puts "*************"
+# gen_data
+welcome_message
+get_input
+# puts ""
+# puts "*************"
+# puts "Highest pay"
+# puts "*************"
+# puts ""
+# print Job.highest_pay
+# puts ""
+# puts "*************"
+# puts "Search by location"
+# puts "*************"
+# print Job.search_by_location("Klockoburgh")
+# puts ""
+# puts "*************"
+# puts "Search by full time"
+# puts "*************"
+# puts ""
+# # binding.pry
+# puts Job.search_by_full_time
+# puts ""
+# puts "*************"
+# puts "Search by part time "
+# puts "*************"
+# puts ""
+# puts Job.search_by_part_time
+# puts ""
+# puts "*************"
+# puts ""
+# # puts Job.highest_pay_part_time
+# puts ""
+# puts "*************"
+# puts ""
+# puts ""
+# puts "*************"
+# print "$#{Organization.first.avg_sal_org}"
+# puts ""
+# puts "*************"
 # attribute_add
